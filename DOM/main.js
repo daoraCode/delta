451f4d9e1b606050f11e6b3f57069044887fdb5a
daoraCode <severin.mboukou@gmail.com>
2020-10-26 12:46:08 +0100

    monday, working around DOM and technics

---
+++ DOM/main.js
@@ -0,0 +1,28 @@
+// console.log('bonjour')
+
+
+
+// Cette action permettre de récupérer ou d'appeler le body afin de le manipuler/le modifier
+let body = document.getElementsByTagName('body')[0]
+console.log(body)
+
+
+
+// Cette action permet de créer un élement dans le body provenant du fichier html
+let newh1 = document.createElement('h1')
+newh1.className = 'myh1'
+//------ au méthode possible --------//
+newh1.setAttribute('id', 'myh1')
+
+
+
+
+// Je dis que l'élément h1 sera le texte Severin
+// De cette manière le prénom créer pour myh1 sera bien un titre h1
+newh1.innerText = 'Severin'
+
+
+
+body.appendChild(newh1)
+//console.log(body)
+// console.log(newh1)
