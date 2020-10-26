cd95585b280db735dd03a044870a2c5062d7e233
daoraCode <severin.mboukou@gmail.com>
2020-10-26 14:56:01 +0100

    how to create some content with js

---
+++ DOM/2/main.js
@@ -0,0 +1,28 @@
+console.log('on working')
+// je crée mon élément li
+let newElement = document.createElement('li')
+// je crée du texte : Coucou, c'est Séverin
+let newText = document.createTextNode('Coucou, c\'est Séverin')
+// je rajoute mon texte dans la variable newElement qui représente un li
+newElement.appendChild(newText)
+// je récupère mon parent ul
+let ul = document.getElementById('list')
+// je sélectionne l'enfant que je souhaite remplacer
+let oldElement = ul.children[1]
+// ---------// -------> let old Element = document.getElementbyI('list').children[1]
+ul.replaceChild(newElement, oldElement)
+
+
+
+
+
+
+// ----------------- PARTIE 2 autre façon ------------------ //
+
+let newElement2 = document.createElement('li')
+
+newElement2.textContent = "Me voilà de retour, c'est encore Séverin"
+
+let ul2 = document.getElementById('list')
+
+let oldElement2 = ul2.children[1    ]
